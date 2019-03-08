f188a0c21cd79541f41e3cfcf3edc776a97b27a5
Justin Retzolk <jretzolk@protonmail.com>
2019-03-08 13:15:57 -0600

    added pbc alias

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -1,6 +1,7 @@
 # Filesystem
 alias l="ls -lAh"
 alias sshkey="cat ~/.ssh/id_rsa.pub | pbcopy && echo 'Copied to clipboard'"
+alias pbc="pbcopy <"
 
 # Docker
 alias dcrun="docker-compose -f /opt/docker-compose.yml"
