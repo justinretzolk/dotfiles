875b2c6c562c0fd04e1de85056cbcf61bc302ab0
justinretzolk <jretzolk@hashicorp.com>
2019-07-17 14:12:49 -0500

    Whoops, missed some things to remove 'thefuck'

--- zsh/extras/alias.zsh
+++ zsh/extras/alias.zsh
@@ -2,9 +2,3 @@
 alias l="ls -lAh"
 alias sshkey="less ~/.ssh/id_rsa.pub | pbcopy && echo 'Copied to clipboard'"
 alias pbc="pbcopy <"
-
-# Docker
-alias dcrun="docker-compose -f /opt/docker-compose.yml"
-
-# Extras
-eval $(thefuck --alias)
