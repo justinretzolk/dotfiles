c44603c3d89aefd37dda7313026ead3998f694b3
justinretzolk <jretzolk@hashicorp.com>
2020-04-18 19:52:15 -0500

    merged work aliases into alias.zsh, removed spicetify

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -2,3 +2,7 @@
 alias l="ls -lAh"
 alias sshkey="cat ~/.ssh/id_rsa.pub | pbcopy && echo 'Copied to clipboard'"
 alias pbc="pbcopy <"
+
+# work
+alias bundle='cd ~/Downloads/; process-bundle ~/HashiCorp/support-bundles/; cd ~/HashiCorp/support-bundles/'
+alias dump='if [[ -e master/replicated/internal/ledis-app.dump ]]; then ptfe-support-tool --dump-file master/replicated/internal/ledis-app.dump | jq . && echo "ledis-dump used"; else jq . master/replicated/internal/app-config.json && echo "app-config used"; fi'
