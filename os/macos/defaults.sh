733b68e896bdd0efda7cb724525e8a61a47c9765
Justin Retzolk <44710313+justinretzolk@users.noreply.github.com>
2020-09-08 13:29:00 -0500

    Finalize iTerm2 config directory move

--- os/macos/defaults.sh
+++ os/macos/defaults.sh
@@ -78,7 +78,7 @@ defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 # ------- #
 
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/os/macos/iterm/"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.config/iterm"
 
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
