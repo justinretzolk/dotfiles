b70292b9c590077ab6fa762abd452c2489d274c8
Justin Retzolk <44710313+justinretzolk@users.noreply.github.com>
2022-01-26 12:31:20 -0600

    Update install script

--- install.sh
+++ install.sh
@@ -1,49 +1,36 @@
 #!/bin/bash
 
-# Determine what type of OS you're running
+# Determine the OS
 OS=$(uname -s)
 
+# macOS configuration
 if [[ $OS == "Darwin" ]]; then
 
-  # install defaults
-  #${PWD}/os/macos/defaults.sh
-
-  # install xcode cli tools
-  # need to test that this actually works before I implement it lol
-  # should probably actually figure out a way to test whether it's already been done.
-  #xcode-select --install
+  # set defaults
+  source ./os/macos/defaults.sh
 
   # make sure brew is installed
-  command -v brew > /dev/null && echo "Homebrew is already installed." || curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install | ruby
+  if ! $(command -v brew &> /dev/null); then
+	  echo "Installing Homebrew..."
+	  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install | ruby
+  fi
 
   # wait for Homebrew to be installed
-  until $(command -v brew > /dev/null 2>&1); do
-    echo "Waiting for Homebrew to be installed."
+  until $(command -v brew &> /dev/null); do
+    echo "Waiting for Homebrew to be installed..."
     sleep 5
   done
 
-  # install brews
+  # install programs via brew
   brew bundle install --file=${PWD}/os/macos/Brewfile
-
-elif [[ $OS == "Linux" ]]; then
-
-  # linux config
-  ${PWD}/os/linux/defaults.sh
-
-  if $(command -v apt > /dev/null 2>&1); then
-    ${PWD}/os/linux/apt.sh
-  else
-    # eventually when I have setups for other distros, I'll add an elif
-    echo "No specific configurations for this OS."
-  fi
 
 else
 
-  echo "OS cannot be determined. Exiting."
+  echo "No configs for this OS: ${OS}"
   exit
 
 fi
 
 # stow the config directory
-# dots are set up for XDG spec, this directory is the only one we need to stow
+# dots are set up for XDG spec, this directory is the only one that needs to be to stowed.
 stow config
