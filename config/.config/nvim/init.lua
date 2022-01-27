afaa5c91c671a5cea96cc154c64de6fb856a75fa
Justin Retzolk <44710313+justinretzolk@users.noreply.github.com>
2022-01-27 10:04:10 -0600

    iteration

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -3,4 +3,4 @@ require('plugins') 	-- install plugins
 require('options') 	-- set configuration options and variables
 require('lsp') 		-- configure the language server and completions
 require('keymaps') 	-- configure additional keybindings
-require('nerdtree')
+require('nerdtree') -- nerdtree specific configs
--- config/.config/nvim/lua/options.lua
+++ config/.config/nvim/lua/options.lua
@@ -1,40 +1,42 @@
 -- things that don't seem to have a native way to do it
-vim.cmd([[colorscheme onedark]])
+local cmd = vim.cmd
+cmd 'colorscheme onedark'
+cmd 'filetype plugin indent on'
+cmd 'syntax on'
 
-vim.cmd([[
-	filetype plugin indent on
-	syntax on
-]])
+-- native globals
+local g = vim.g
+g.airline_theme = 'onedark'
+g.airline_powerline_fonts = 1
+g.better_whitespace_enabled = 1
+g.mapleader = ','
+g.NERDTreeShowHidden = 1
 
--- native things
-vim.g.airline_theme = 'onedark'
-vim.g.airline_powerline_fonts = 1
-vim.g.better_whitespace_enabled = 1
-vim.g.mapleader = ','
-vim.g.NERDTreeShowHidden = 1
-vim.opt.autoindent = true
-vim.opt.backspace = { 'indent', 'eol', 'start' }
-vim.opt.backup = false
-vim.opt.clipboard = 'unnamed'
-vim.opt.colorcolumn = { '81', '121' }
-vim.opt.compatible = false
-vim.opt.completeopt = { 'menuone', 'noselect' }
-vim.opt.cursorline = true
-vim.opt.errorbells = false
-vim.opt.expandtab = false
-vim.opt.foldenable = true
-vim.opt.hidden = true
-vim.opt.hlsearch = true
-vim.opt.ignorecase = true
-vim.opt.incsearch = true
-vim.opt.modifiable = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
-vim.opt.showmode = false
-vim.opt.smartcase = true
-vim.opt.smartindent = true
-vim.opt.tabstop = 4
-vim.opt.termguicolors = true
-vim.opt.wrap = true
-vim.opt.writebackup = false
+-- local opts
+local opt = vim.opt
+opt.autoindent = true
+opt.backspace = { 'indent', 'eol', 'start' }
+opt.backup = false
+opt.clipboard = 'unnamed'
+opt.colorcolumn = { '81', '121' }
+opt.compatible = false
+opt.completeopt = { 'menuone', 'noselect' }
+opt.cursorline = true
+opt.errorbells = false
+opt.expandtab = false
+opt.foldenable = true
+opt.hidden = true
+opt.hlsearch = true
+opt.ignorecase = true
+opt.incsearch = true
+opt.modifiable = true
+opt.number = true
+opt.relativenumber = true
+opt.shiftwidth = 4
+opt.showmode = false
+opt.smartcase = true
+opt.smartindent = true
+opt.tabstop = 4
+opt.termguicolors = true
+opt.wrap = true
+opt.writebackup = false
