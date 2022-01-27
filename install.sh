0bff49cce8bfcba14fa893e44de7b277a7d53b44
Justin Retzolk <44710313+justinretzolk@users.noreply.github.com>
2022-01-27 11:40:48 -0600

    Add language server install to install.sh

--- install.sh
+++ install.sh
@@ -6,28 +6,48 @@ OS=$(uname -s)
 # macOS configuration
 if [[ $OS == "Darwin" ]]; then
 
-  # set defaults
-  source ./os/macos/defaults.sh
+	# set defaults
+	echo "Setting up defaults..."
+	source ./os/macos/defaults.sh
 
-  # make sure brew is installed
-  if ! $(command -v brew &> /dev/null); then
-	  echo "Installing Homebrew..."
-	  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install | ruby
-  fi
+	# make sure brew is installed
+	if ! $(command -v brew &> /dev/null); then
+		echo "Installing Homebrew..."
+		curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install | ruby
+	fi
 
-  # wait for Homebrew to be installed
-  until $(command -v brew &> /dev/null); do
-    echo "Waiting for Homebrew to be installed..."
-    sleep 5
-  done
+	# wait for Homebrew to be installed
+	until $(command -v brew &> /dev/null); do
+		echo "Waiting for Homebrew to be installed..."
+		sleep 5
+	done
 
-  # install programs via brew
-  brew bundle install --file=${PWD}/os/macos/Brewfile
+	# install programs via brew
+	echo "Installing brews..."
+	brew bundle install --file=${PWD}/os/macos/Brewfile
+
+	# bash lsp install
+	if ! $(command -v bash-language-server &> /dev/null); then
+		echo "Installing bash language server..."
+		npm i -g bash-language-server
+	fi
+
+	# json lsp install
+	if ! $(command -v vscode-json-language-server &> /dev/null); then
+		echo "Installing json language server..."
+		npm i -g vscode-langservers-extracted
+	fi
+
+	# yaml lsp install
+	if ! $(command -v yaml-language-server &> /dev/null); then
+		echo "Installing yaml language server..."
+		yarn global add yaml-language-server
+	fi
 
 else
 
-  echo "No configs for this OS: ${OS}"
-  exit
+	echo "No configs for this OS: ${OS}"
+	exit
 
 fi
 
